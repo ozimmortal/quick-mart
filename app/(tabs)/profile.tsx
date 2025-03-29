e7858b7415475223cdedcf5abc07169c27daeff8
oz <zaleambo@gmail.com>
2025-03-29 21:23:55 +0300

    fixed the login page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
-import { Settings, CreditCard, Package, Heart, Bell, CircleHelp as HelpCircle, LogOut } from 'lucide-react-native';
+import { Settings, CreditCard, Package, Heart, Bell, CircleHelp as HelpCircle, LogOut,LogIn } from 'lucide-react-native';
 
 const MENU_ITEMS = [
   { icon: Settings, label: 'Settings', color: '#4A90E2' },
@@ -11,39 +11,62 @@ const MENU_ITEMS = [
 ];
 
 export default function ProfileScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          source={{ uri: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&q=80&w=1000' }}
-          style={styles.avatar}
-        />
-        <Text style={styles.name}>John Doe</Text>
-        <Text style={styles.email}>john.doe@example.com</Text>
+  let login = false;
+  if(login){
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={{ uri: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&q=80&w=1000' }}
+            style={styles.avatar}
+          />
+          <Text style={styles.name}>John Doe</Text>
+          <Text style={styles.email}>john.doe@example.com</Text>
+        </View>
+  
+        <View style={styles.menuContainer}>
+          {MENU_ITEMS.map((item, index) => (
+            <TouchableOpacity key={index} style={styles.menuItem}>
+              <View style={[styles.iconContainer, { backgroundColor: `${item.color}15` }]}>
+                <item.icon size={24} color={item.color} />
+              </View>
+              <Text style={styles.menuLabel}>{item.label}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+  
+        <TouchableOpacity style={styles.logoutButton}>
+          <LogOut size={24} color="#FF385C" />
+          <Text style={styles.logoutText}>Log Out</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
+  else{
+    return (
+      <View style={styles.containerIn}>
+        <TouchableOpacity style={styles.logoutButton}>
+          <LogIn size={24} color="#38FF60FF" />
+          <Text style={styles.logoutText}>Log In</Text>
+        </TouchableOpacity>
       </View>
-
-      <View style={styles.menuContainer}>
-        {MENU_ITEMS.map((item, index) => (
-          <TouchableOpacity key={index} style={styles.menuItem}>
-            <View style={[styles.iconContainer, { backgroundColor: `${item.color}15` }]}>
-              <item.icon size={24} color={item.color} />
-            </View>
-            <Text style={styles.menuLabel}>{item.label}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <TouchableOpacity style={styles.logoutButton}>
-        <LogOut size={24} color="#FF385C" />
-        <Text style={styles.logoutText}>Log Out</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  );
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  containerIn: {
+    display:"flex",
+    width:"100%",
+    height:"100%",
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center",
     backgroundColor: '#fff',
   },
   header: {
